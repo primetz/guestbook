e86888a7f54ca247e748f387861e53caf25d914c
Stanislav Rodikov <stanislav.rodikov@yahoo.com>
2023-01-08 13:15:53 +0300

    Building an SPA

---
+++ app/spa/src/pages/home.js
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+import { Link } from "preact-router";
+
+export default function Home({conferences}) {
+    if (!conferences) {
+        return <div className="p-3 text-center">No conferences yet</div>
+    }
+
+    return (
+        <div className="p-3">
+            {conferences.map((conference) => (
+                <div className="card border shadow-sm lift mb-3">
+                    <div className="card-body">
+                        <div className="card-title">
+                            <h4 className="font-weight-light">
+                                {conference.city} {conference.year}
+                            </h4>
+                        </div>
+
+                        <Link href={'/conference/' + conference.slug} className="btn btn-sm btn-primary stretched-link">View</Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
